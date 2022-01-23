ba00cb2b69720fd1c798db3d43259ba510829380
Steven Michael <ssmichael@gmail.com>
2022-01-23 17:37:12 -0500

    setup for github pages deploy

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production'
+
+
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/burdle/' : ''
+}
