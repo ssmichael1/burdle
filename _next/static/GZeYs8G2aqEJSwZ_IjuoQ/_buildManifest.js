7ba1e87cb759dab30e2453c8b38db439a6a302ab
StevenSamirMichael <StevenSamirMichael@users.noreply.github.com>
2022-01-24 22:46:55 +0000

    deploy: 9165ada1aad6561fb2f3e53283a203b07550d268

--- _next/static/GZeYs8G2aqEJSwZ_IjuoQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/723-502b5d2069b5bd55.js","static/chunks/pages/index-3c9fced2e90af770.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
