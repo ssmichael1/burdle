126b2492d0d0bde1e31c204ae061560403f849fa
Steven Michael <ssmichael@gmail.com>
2022-01-24 11:05:54 -0500

    make better use of react...

--- components/gamegrid.module.tsx
+++ components/gamegrid.module.tsx
@@ -1,6 +1,7 @@
 import React, { NewLifecycle } from 'react'
 import dict from './words5.json'
 import { Modal, Button } from 'react-bootstrap'
+import { takeCoverage } from 'v8';
 
 const keyboard = [
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
@@ -8,18 +9,40 @@ const keyboard = [
     ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BS']
 ]
 
-interface Row {
-    vals: Array<string | null>
-    ref: React.RefObject<HTMLDivElement>;
+type GameStatus = 'playing' | 'win' | 'loss';
+type LetterStatus = 'notplayed' | 'played' | 'match' | 'inword' | 'nomatch'
+
+interface Letter {
+    val: string;
+    status: LetterStatus;
 }
+
+interface Row {
+    letters: Array<Letter>;
+    shake: boolean;
+}
+
 
 interface GameState {
     showModal: boolean;
+    rows: Array<Row>;
+    status: GameStatus;
+    theword: string;
 }
+
 interface GameProps { }
 
-type GameStatus = 'playing' | 'win' | 'loss';
+interface RowState { };
+interface RowProps {
+    val: Row;
+    idx: number;
+};
 
+interface LetterProps {
+    val: Letter;
+    idx: number;
+}
+interface LetterState { };
 
 export const gamerows = 6
 export const wordlen = 5
@@ -28,14 +51,95 @@ const onlyUnique = (value: any, index: number, self: Array<any>) => {
     return self.indexOf(value) === index;
 }
 
+class GameLetter extends React.Component<LetterProps, LetterState> {
+    ref: React.RefObject<HTMLDivElement>
+
+    constructor(props: LetterProps) {
+        super(props)
+        this.ref = React.createRef()
+    }
+
+    render() {
+        let c = "letter column";
+        if (this.props.val.status == 'played') {
+            c += " filled"
+        }
+        if ((this.props.val.status === 'match') ||
+            (this.props.val.status === 'inword') ||
+            (this.props.val.status === 'nomatch')) {
+            setTimeout(() => {
+                if (this.ref.current === null)
+                    return
+                if (this.props.val.status == 'match') {
+                    this.ref.current.classList.add('match')
+                }
+                else if (this.props.val.status == 'inword') {
+                    //c += ` close${this.props.idx}`
+                    this.ref.current.classList.add('close')
+                }
+                else if (this.props.val.status == 'nomatch') {
+                    //c += ` nomatch${this.props.idx}`
+                    this.ref.current.classList.add('nomatch')
+                }
+            }, this.props.idx * 200)
+        }
+
+        return (
+            <div
+                className={c}
+                ref={this.ref}
+            >
+                {this.props.val.val == '' ? null : this.props.val.val}
+            </div>
+        )
+    }
+
+}
+
+class GameRow extends React.Component<RowProps, RowState> {
+
+    ref: React.RefObject<HTMLDivElement>;
+
+    constructor(props: RowProps) {
+        super(props)
+        this.ref = React.createRef()
+    }
+
+    render() {
+        let c = "row"
+
+        // Shake, but remove class so it can shake again
+        // later if needed
+        if (this.props.val.shake == true) {
+            c += " shake"
+        }
+        return (
+            <div className={c}
+                key={`row${this.props.idx}`}
+                ref={this.ref}
+            >
+                {
+                    // Render letters
+                    this.props.val.letters.map((v, i) => {
+                        return (
+                            <GameLetter
+                                val={v}
+                                idx={i}
+                                key={`row${this.props.idx}_letter${i}`}
+                            />
+                        )
+                    })
+                }
+            </div >
+        )
+    }
+}
+
 export default class GameGrid extends React.Component<GameProps, GameState> {
 
     cur_row: number;
     cur_letter: number;
-    rows: Array<Row>
-    theword: string;
     modal: any;
-    status: GameStatus;
 
     match: Array<string>
     close: Array<string>
@@ -43,35 +147,46 @@ export default class GameGrid extends React.Component<GameProps, GameState> {
 
     modal_title() {
         return (
-            this.status == 'win' ?
+            this.state.status == 'win' ?
                 'You Win' : 'You Lose'
         )
     }
 
     modal_body() {
         return (
-            this.status == 'win' ?
-                <div>Word is {this.theword}</div> :
-                <div>Word is {this.theword}</div>
+            this.state.status == 'win' ?
+                <div>Word is {this.state.theword}</div> :
+                <div>Word is {this.state.theword}</div>
         )
+    }
+
+    static emptyrow(shake: boolean = false): Row {
+
+        return {
+            letters: Array.from(Array(wordlen)).map((w, i2) => {
+                return {
+                    val: '',
+                    status: 'notplayed'
+
+                }
+            }),
+            shake: shake
+        }
     }
 
     constructor(props: GameProps) {
         super(props)
 
-        this.rows = Array.from(Array(gamerows)).map((v, i) => {
-            return {
-                vals: Array(wordlen).fill(null),
-                ref: React.createRef()
-            }
-        })
         this.cur_row = 0
         this.cur_letter = 0
-        this.theword = 'steve'
         this.state = {
-            showModal: false
+            showModal: false,
+            rows: Array.from(Array(gamerows)).map((v, i) => {
+                return GameGrid.emptyrow()
+            }),
+            theword: 'steve',
+            status: 'playing'
         }
-        this.status = 'playing'
         this.match = []
         this.close = []
         this.nomatch = []
@@ -88,13 +203,6 @@ export default class GameGrid extends React.Component<GameProps, GameState> {
         //document.getElementById("exampleModal")?.close()
     };
 
-    update() {
-        this.setState(
-            {
-                showModal: this.state.showModal,
-            }
-        )
-    }
 
     drawKeys(): any {
         return keyboard.map((keyrow, i) => {
@@ -133,66 +241,47 @@ export default class GameGrid extends React.Component<GameProps, GameState> {
         })
     }
 
-    renderRow(row: Row, rownum: number): any {
-
-        return (
-            <div className="row" key={rownum} ref={this.rows[rownum].ref}>
-                {row.vals.map((v, i) => {
-                    let c = "letter column";
-                    return (
-                        <div key={rownum * 10 + i}
-                            className={c}
-                        >
-                            {(v === null) ? '' : v}
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    }
 
     onEnter() {
         // Enter only valid at end of line
         if (this.cur_letter != wordlen)
             return
 
-        // Get current row
-        let row = this.rows[this.cur_row]
-        let ref = this.rows[this.cur_row].ref
+        let newstate = this.state
 
-        // Get the testword
-        let testword = row.vals.join('').toLowerCase()
-        if (ref.current === null)
-            return;
+        // Get current row
+        let row = newstate.rows[this.cur_row];
+
+        // Get the testword    
+        let testword = row.letters.reduce((s, letter, i) => {
+            return s + letter.val
+        }, '').toLowerCase()
 
         // Not a word!
         if (!dict.includes(testword)) {
             //this.cur_row = this.cur_row + 1;
             this.cur_letter = 0;
-            this.rows[this.cur_row].vals = Array(wordlen).fill(null)
-            Array.from(Array(wordlen)).map((v, i) => {
-                ref.current?.children.item(i)?.classList.remove("filled")
-            }
-            )
-            ref.current.classList.add("shake")
-            setTimeout(() => { ref.current?.classList.remove("shake") }, 500)
-            this.update()
+            this.state.rows[this.cur_row] =
+                GameGrid.emptyrow(true)
+            this.setState(this.state)
+            // Turn off shake after 250 ms
+            setTimeout(() => {
+                this.state.rows[this.cur_row] =
+                    GameGrid.emptyrow(false)
+                this.setState(this.state)
+            }, 250)
             return
         }
 
         // Got a word
-        // Remove 'filled' class
-        Array.from(Array(wordlen)).map((v, i) => {
-            ref.current?.children.item(i)?.classList.remove("filled")
-        })
-
         let localmatches: Array<string> = []
 
         // Look for exact matches
         Array.from(Array(wordlen)).map((v, i) => {
             let letter = testword.slice(i, i + 1)
-            if (letter === this.theword.slice(i, i + 1)) {
-                ref.current?.children.item(i)?.classList.add('match' + i)
+            let theword = this.state.theword;
+            if (letter === theword.slice(i, i + 1)) {
+                row.letters[i].status = 'match'
                 this.match.push(testword.slice(i, i + 1).toUpperCase())
                 localmatches.push(letter)
             }
@@ -200,15 +289,16 @@ export default class GameGrid extends React.Component<GameProps, GameState> {
 
         Array.from(Array(wordlen)).map((v, i) => {
             let letter = testword.slice(i, i + 1)
+            let theword = this.state.theword
 
             // CLose match but not previous exact
-            if ((this.theword.includes(letter)) && (!localmatches.includes(letter))) {
-                ref.current?.children.item(i)?.classList.add('close' + i)
+            if ((theword.includes(letter)) && (!localmatches.includes(letter))) {
+                row.letters[i].status = 'inword'
                 this.close.push(letter.toUpperCase())
             }
             // no matches
-            else if (letter != this.theword.slice(i, i + 1)) {
-                ref.current?.children.item(i)?.classList.add('nomatch' + i)
+            else if (letter != theword.slice(i, i + 1)) {
+                row.letters[i].status = 'nomatch'
                 this.nomatch.push(letter.toUpperCase())
             }
 
@@ -218,57 +308,64 @@ export default class GameGrid extends React.Component<GameProps, GameState> {
         this.nomatch = this.nomatch.filter(onlyUnique)
 
         // Won Game!
-        if (testword == this.theword) {
-            Array.from(Array(wordlen)).map((v, i) => {
-                ref.current?.children.item(i)?.classList.add("match")
-            })
-            this.status = 'win'
+        if (testword == newstate.theword) {
             setTimeout(this.showModal.bind(this), 1100)
+            this.setState({
+                ...this.state,
+                status: 'win'
+            })
+            return
         }
         // Lost Game!
         else if (this.cur_row == gamerows - 1) {
-            this.status = 'loss'
+            this.setState({
+                ...this.state,
+                status: 'loss'
+            })
             setTimeout(this.showModal.bind(this), 1100)
         }
         // on to the next row
         else {
             this.cur_row = this.cur_row + 1
             this.cur_letter = 0
+            this.setState(newstate)
         }
-
 
     }
 
     handleKey(key: string) {
-        if (this.status !== 'playing')
+        if (this.state.status !== 'playing')
             return
 
-        let row = this.rows[this.cur_row]
-        let ref = this.rows[this.cur_row].ref
+        let row = this.state.rows[this.cur_row]
 
         // Check for new backspace
         if (key === 'Backspace') {
             if (this.cur_letter > 0) {
-                this.rows[this.cur_row].vals[this.cur_letter - 1] = null
-                ref?.current?.children?.item(this.cur_letter - 1)?.classList.remove("filled")
+                row.letters[this.cur_letter - 1].val = ''
+                row.letters[this.cur_letter - 1].status = 'notplayed'
                 this.cur_letter -= 1;
+                this.setState(this.state)
             }
+            return
         }
         // Check for Enter
         else if (key == 'Enter') {
             this.onEnter()
+            return
         }
 
         key = key.toUpperCase()
         // Check for new letter
         if ((key.length == 1) && (/[A-Z]/.test(key))) {
             if (this.cur_letter < wordlen) {
-                row.vals[this.cur_letter] = key
-                ref?.current?.children?.item(this.cur_letter)?.classList.add("filled")
+                row.letters[this.cur_letter].val = key
+                row.letters[this.cur_letter].status = 'played'
                 this.cur_letter += 1;
+                this.setState(this.state)
+                return
             }
         }
-        this.update()
     }
 
     onKeyDown(e: any) {
@@ -282,10 +379,15 @@ export default class GameGrid extends React.Component<GameProps, GameState> {
 
     componentDidMount() {
         document.addEventListener("keydown", this.onKeyDown.bind(this))
-        // Ughh... need to do this only on client or server
+        // Ughh... need to do this only on client
+        // or different random values flag an error 
         let idx = Math.floor(Math.random() * dict.length)
-        this.theword = dict[idx];
-        this.update()
+        this.setState(
+            {
+                ...this.state,
+                theword: dict[idx]
+            }
+        )
     }
 
 
@@ -297,8 +399,18 @@ export default class GameGrid extends React.Component<GameProps, GameState> {
 
         let html = (
             <div>
-                <div key='3' className="justify-content-center gamegrid">
-                    {this.rows.map((r, i) => this.renderRow(r, i))}
+                <div key='gamegrid' className="justify-content-center gamegrid">
+                    {
+                        // Render rows
+                        Array.from(Array(gamerows)).map((v, i) => {
+                            return (
+                                <GameRow
+                                    key={'row' + i}
+                                    val={this.state.rows[i]}
+                                    idx={i} />
+                            )
+                        })
+                    }
                 </div >
                 <div className="pt-5"></div>
                 <div className="container justify-content-center keyboard">
