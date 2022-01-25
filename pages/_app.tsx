fba6da48105372596123537c67d7d2379e6757da
Steven Michael <ssmichael@gmail.com>
2022-01-24 19:32:21 -0500

    remove react-ga ; make analytics ID an environment var

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,19 +22,22 @@ function MyApp({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  //return <Component {...pageProps} />
-  return (
-    <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+  if (gtag.GA_TRACKING_ID === undefined)
+    return <Component {...pageProps} />
+
+  else
+    return (
+      <>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -42,11 +45,12 @@ function MyApp({ Component, pageProps }: AppProps) {
               page_path: window.location.pathname,
             });
           `,
-        }}
-      />
-      <Component {...pageProps} />
-    </>
-  )
+          }}
+        />
+        <Component {...pageProps} />
+      </>
+    )
+
 }
 
 export default MyApp
